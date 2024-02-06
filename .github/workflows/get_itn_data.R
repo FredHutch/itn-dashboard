b288514b736c1f15b4f79c4e3f9f4f2c28e291df
Candace Savonen <cansav09@gmail.com>
2024-02-06 10:33:44 -0500

    Fix file paths.

--- .github/workflows/get_itn_data.R
+++ .github/workflows/get_itn_data.R
@@ -1,9 +1,11 @@
 # C. Savonen
 
 # Download that data
+# Find .git root directory
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 
-dir.create("data", showWarnings = FALSE)
-dir.create("plots", showWarnings = FALSE)
+dir.create(file.path(root_dir, "data"), showWarnings = FALSE)
+dir.create(file.path(root_dir, "plots"), showWarnings = FALSE)
 
 set.seed(1234)
 
@@ -37,19 +39,19 @@ ga_metrics <- dplyr::bind_rows(fhdsl_stats_list$metrics ,itcr_stats_list$metrics
   dplyr::filter(
     !(website %in%not_itcr)
     )
-saveRDS(ga_metrics, file.path("data","itcr_ga_metric_data.RDS"))
+saveRDS(ga_metrics, file.path(root_dir, "data","itcr_ga_metric_data.RDS"))
 
 ga_dims <- dplyr::bind_rows(fhdsl_stats_list$dimensions, itcr_stats_list$dimensions) %>%
   dplyr::filter(
     !(website %in% not_itcr)
     )
-saveRDS(ga_dims, file.path("data","itcr_ga_dims_data.RDS"))
+saveRDS(ga_dims, file.path(root_dir, "data","itcr_ga_dims_data.RDS"))
 
 ga_link_clicks <- dplyr::bind_rows(fhdsl_stats_list$link_clicks,itcr_stats_list$link_clicks) %>%
   dplyr::filter(
     !(website %in% not_itcr)
     )
-saveRDS(ga_link_clicks, file.path("data","itcr_ga_link_click_data.RDS"))
+saveRDS(ga_link_clicks, file.path(root_dir, "data","itcr_ga_link_click_data.RDS"))
 
 manual_course_info <- googlesheets4::read_sheet(
   "https://docs.google.com/spreadsheets/d/1-8vox2LzkVKzhmSFXCWjwt3jFtK-wHibRAq2fqbxEyo/edit#gid=1550012125", sheet = "Course_data",
@@ -75,25 +77,25 @@ web_traffic_overtime <- ga_dims %>%
                                            TRUE ~ website))
 
 # Save these to TSVs
-readr::write_tsv(itcr_course_data, file.path("data", "itcr_course_metrics.tsv"))
-readr::write_tsv(web_traffic_overtime, file.path("data", "web_traffic_overtime.tsv"))
+readr::write_tsv(itcr_course_data, file.path(root_dir,"data", "itcr_course_metrics.tsv"))
+readr::write_tsv(web_traffic_overtime, file.path(root_dir,"data", "web_traffic_overtime.tsv"))
 
 collabs <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1-8vox2LzkVKzhmSFXCWjwt3jFtK-wHibRAq2fqbxEyo/edit#gid=0")
 
 # Save this to a TSV
-readr::write_tsv(collabs, file.path("data", "collabs.tsv"))
+readr::write_tsv(collabs, file.path(root_dir, "data", "collabs.tsv"))
 
 loqui_usage <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1G_HTU-bv2k5txExP8EH3ScUfGqtW1P3syThD84Z-g9k/edit#gid=0")
 
 # Save this to a TSV
-readr::write_tsv(loqui_usage, file.path("data", "loqui_usage.tsv"))
+readr::write_tsv(loqui_usage, file.path(root_dir,"data", "loqui_usage.tsv"))
 
 itcr_drive_id <- "https://drive.google.com/drive/folders/0AJb5Zemj0AAkUk9PVA"
 itcr_slido_data <- get_slido_files(itcr_drive_id)
-saveRDS(itcr_slido_data, file.path("data", "itcr_slido_data.RDS"))
+saveRDS(itcr_slido_data, file.path(root_dir, "data", "itcr_slido_data.RDS"))
 
 career_stage_counts <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1-8vox2LzkVKzhmSFXCWjwt3jFtK-wHibRAq2fqbxEyo/edit#gid=8290691", range = "Workshop attendee type")
-readr::write_tsv(career_stage_counts, file.path("data", "career_stage_counts.tsv"))
+readr::write_tsv(career_stage_counts, file.path(root_dir, "data", "career_stage_counts.tsv"))
 
 repos <- c(
   "fhdsl/metricminer",
@@ -128,6 +130,6 @@ repos <- c(
 
 gh_metrics <- get_repos_metrics(repo_names = repos)
 
-saveRDS(gh_metrics, file.path("data", "itcr_gh_metrics.RDS"))
+saveRDS(gh_metrics, file.path(root_dir,"data", "itcr_gh_metrics.RDS"))
 
 sessionInfo()
