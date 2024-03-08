9b2698d15b31ac74c3d4646e202a15baba27337d
Howard Baek <50791792+howardbaek@users.noreply.github.com>
2024-03-07 16:58:24 -0800

    Fix bug

--- .github/workflows/get_itn_data.R
+++ .github/workflows/get_itn_data.R
@@ -74,7 +74,7 @@ readr::write_csv(itcr_course_metrics, file.path(root_dir,"data", "itcr_course_me
 # ITCR Google Drive
 itcr_drive_id <- "https://drive.google.com/drive/folders/0AJb5Zemj0AAkUk9PVA"
 itcr_slido_data_raw <- get_slido_files(itcr_drive_id)
-itcr_slido_data <- itcr_slido_data$`Polls-per-user`
+itcr_slido_data <- itcr_slido_data_raw$`Polls-per-user`
 
 # Save these to CSVs
 readr::write_csv(itcr_slido_data, file.path(root_dir,"data", "itcr_slido_data.csv"))
